ac16f26933fa31bd15ba677ca35c7bc4ac2602f8
Akai-Kumako <e1433@s.akashi.ac.jp>
2017-07-12 00:24:43 +0900

    演習問題10-2 完了

--- Graph.java
+++ Graph.java
@@ -1,9 +1,19 @@
 public class Graph extends Turtle{
-	public int origin_x;
-	public int origin_y;
+	private int origin_x = 0;
+	private int origin_y = 0;
 	public void drawlines(int n, int x[], int y[]){
 		for(int i = 0; i < n; i++){
 			moveTo(x[i] + origin_x, -y[i] + origin_y);
 		}
+	}
+	public void setOrigin(int origin_x, int origin_y){
+		this.origin_x = origin_x;
+		this.origin_y = origin_y;
+	}
+	public int getOriginX(){
+		return origin_x;
+	}
+	public int getOriginY(){
+		return origin_y;
 	}
 }
--- GraphMain.java
+++ GraphMain.java
@@ -1,26 +1,26 @@
 public class GraphMain{
- public static void main(String[] args){
- 	int x0=20, y0=200, xmin=0, xmax=360, ymin=-180, ymax=180;
-	TurtleFrame f = new TurtleFrame();
-	Graph m = new Graph(); f.add(m);
+	public static void main(String[] args){
+	 	int x0=20, y0=200, xmin=0, xmax=360, ymin=-180, ymax=180;
+		TurtleFrame f = new TurtleFrame();
+		Graph m = new Graph(); f.add(m);
 
-	m.setColor(java.awt.Color.red);
-	m.up(); m.moveTo(xmin+x0, y0, 0);
-	m.down(); m.moveTo(xmax+x0, y0, 180);
-	m.up(); m.moveTo(x0, ymin+y0, 90);
-	m.down(); m.moveTo(x0, ymax+y0, 90);
-	m.up(); m.moveTo(x0, y0, 90); m.down();
-	m.setColor(java.awt.Color.black);
+		m.setOrigin(20, 200);
+		System.out.println("Origin(" + m.getOriginX() + "," + m.getOriginY() + ")");
 
-	m.origin_x = 20;
-	m.origin_y = 200;
+		m.setColor(java.awt.Color.red);
+		m.up(); m.moveTo(xmin+x0, y0, 0);
+		m.down(); m.moveTo(xmax+x0, y0, 180);
+		m.up(); m.moveTo(x0, ymin+y0, 90);
+		m.down(); m.moveTo(x0, ymax+y0, 90);
+		m.up(); m.moveTo(x0, y0, 90); m.down();
+		m.setColor(java.awt.Color.black);
 
-	int n=51;
-	int[] x = new int[n], y = new int[n];
-	for(int i=0; i<n; i++){
-		x[i]=7*i;
-		y[i]=(int)(100*Math.sin(2*Math.PI*i/(n-1)));
+		int n=51;
+		int[] x = new int[n], y = new int[n];
+		for(int i=0; i<n; i++){
+			x[i]=7*i;
+			y[i]=(int)(100*Math.sin(2*Math.PI*i/(n-1)));
+		}
+		m.drawlines(n, x, y);
 	}
-	m.drawlines(n, x, y);
- }
 }
